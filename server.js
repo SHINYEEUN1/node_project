47cee4750a4a5b088b26f675e9f4330c50d1f39b
SHINYEEUN1 <sh.yeeun101@gmail.com>
2026-04-23 10:37:19 +0900

    DB연결기능구현

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+// 1. routes폴더 생성 -> user.js 파일 생성 (목록조회)
+// 2. config 폴더 생성 -> db.js 파일 생성
+//    .env 파일에 DB정보 기록
+
+const userRouter = require('./routes/user');
+
 const dataset= [
     {"f_name":"귤", "count": 10},
     {"f_name":"사과", "count": 8},
@@ -10,6 +16,7 @@ const dataset= [
 ];
 
 app.use(cors());
+app.use("/users", userRouter);
 
 app.get("/api/list", (req, res)=>{
     console.log("요청이 들어옴...");
